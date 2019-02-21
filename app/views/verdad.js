d1eb66cbc08061354fe0d273adb00ce7982fed88
Justin Rombawa <jusrombawa@gmail.com>
2019-02-21 22:24:59 +0800

    login rework 4
    - Updated database structure, now includes users, reviewers, and affiliations/occupations
    
    - Login for mobile sidenav now works.

--- app/views/verdad.js
+++ app/views/verdad.js
@@ -139,10 +139,49 @@ $(document).ready(function(){
 
     //login form submit for desktop
 
-    //$(document).on("click","#loginButtonDesktop").click(function(e){
     $("#loginButtonDesktop").click(function(e){
         var username = $("#loginUsernameDesktop").val().trim();
         var password = $("#loginPasswordDesktop").val().trim();
+
+        //alert("Logging in as " + username + " with password " + password); //THIS IS FOR TEST PURPOSE ONLY OH MY GOD I SWEAR IF YOU FORGET TO REMOVE THIS
+
+        //check if non-empty login
+        if(username != '' && password != '')
+        {
+            $.ajax({
+                type:"POST",
+                url:"/login",
+                data: {
+                    "username": username,
+                    "password": password
+                },
+
+                success: function(data)
+                {
+                    window.location.reload();
+                },
+
+                error: function(jqXHR, exception)
+                {
+                    alert(jqXHR.responseText);
+                }
+            });
+        }
+
+        else if (username == '')
+            alert("E-mail field cannot be empty.");
+
+        else if (password == '')
+            alert("Password field cannot be empty.");
+
+
+        e.preventDefault(); // avoid to execute the actual submit of the form.
+
+    });
+
+    $("#loginButtonMobile").click(function(e){
+        var username = $("#loginUsernameMobile").val().trim();
+        var password = $("#loginPasswordMobile").val().trim();
 
         //alert("Logging in as " + username + " with password " + password); //THIS IS FOR TEST PURPOSE ONLY OH MY GOD I SWEAR IF YOU FORGET TO REMOVE THIS
 
@@ -213,5 +252,25 @@ $(document).ready(function(){
 
         });
     });
+
+
+    $(document).on("click", "#logoutMobile", function(){
+
+        $.ajax({
+            type: 'POST',
+            url: '/logout',
+            success: function(data) {
+                window.location.reload();
+            },
+
+            error: function(jqXHR, exception)
+            {
+                alert("Error logging out.");
+                alert(jqXHR.responseText);
+            }
+
+        });
+    });
+
 
 });
